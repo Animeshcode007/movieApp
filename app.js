fd04681e73da6c488e7cc531b1bf11b3efa78ac4
Animesh Khare <138273117+Animeshcode007@users.noreply.github.com>
2024-10-30 13:35:22 +0530

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const searchInput = document.getElementById('searchInput');
+const filterRatingButton = document.getElementById('filterRatingButton');
 const movieCards = document.querySelectorAll('.card');
 
 searchInput.addEventListener('input', function () {
@@ -7,8 +8,19 @@ searchInput.addEventListener('input', function () {
     movieCards.forEach(card => {
         const movieName = card.getAttribute('data-name').toLowerCase();
 
-        // Show card if it matches the search query, otherwise hide it
         if (movieName.includes(searchQuery)) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+});
+
+filterRatingButton.addEventListener('click', function () {
+    movieCards.forEach(card => {
+        const movieRating = parseFloat(card.getAttribute('data-rating'));
+
+        if (movieRating >= 8.0) {
             card.style.display = 'block';
         } else {
             card.style.display = 'none';
